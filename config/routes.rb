939ae324d941b7de4bb8b06c3a4358c8ee820a48
Akkiros <kwj96966@gmail.com>
2017-02-24 23:07:40 +1300

    accounts route 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/accounts/' => 'accounts#index'
+  get '/accounts/new' => 'accounts#new'
+  post '/accounts/create' => 'accounts#create'
+  post '/accounts/login' => 'accounts#login'
+  get '/accounts/logout' => 'accounts#new'
 end
