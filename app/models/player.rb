8f1fe29936eac1477dffb9fd5f3332f23c86425d
Akkiros <kwj96966@gmail.com>
2017-02-25 12:20:34 +1300

    누락된 player model validation 추가

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ApplicationRecord
   has_many :game_players
   has_many :games, through: :game_players
+
+  validates :username, presence: true, uniqueness: true, legnth: { maximum: 32 }
+  validates :password, presence: true, legnth: { minimum: 8, maximum: 64 }
 end
