b239bb3c91b83fe9c9c14cee8b0b15ec345125f8
Akkiros <kwj96966@gmail.com>
2017-02-25 12:53:45 +1300

    player model length validator 오타 수정

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,6 @@ class Player < ApplicationRecord
   has_many :game_players
   has_many :games, through: :game_players
 
-  validates :username, presence: true, uniqueness: true, legnth: { maximum: 32 }
-  validates :password, presence: true, legnth: { minimum: 8, maximum: 64 }
+  validates :username, presence: true, uniqueness: true, length: { maximum: 32 }
+  validates :password, presence: true, length: { minimum: 8, maximum: 64 }
 end
