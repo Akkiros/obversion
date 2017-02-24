09ccbf3be3062c4be495cf143d3f1be3cf1c4d91
Akkiros <kwj96966@gmail.com>
2017-02-24 21:40:26 +1300

    game model 추가

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224082804) do
+ActiveRecord::Schema.define(version: 20170224083952) do
+
+  create_table "games", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "status",     limit: 16
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
 
   create_table "players", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "username",   limit: 32
