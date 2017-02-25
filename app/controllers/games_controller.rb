e0decb98edb757ef2f8d262d6250d64e0ff2c0c4
Akkiros <kwj96966@gmail.com>
2017-02-25 14:23:24 +1300

    game controller 에외 처리 추가

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,17 +19,35 @@ class GamesController < ApplicationController
     game = Game.find_by(id: params[:game_id])
     player = Player.find_by(id: session[:player_id])
 
-    # TODO : check game is full
-    game.game_players.create(player: player)
+    if game.nil?
+      puts 'game is not found'
+      return redirect_to action: 'index'
+    end
 
+    if player.nil?
+      puts 'player is not found'
+      return redirect_to accounts_path
+    end
 
-    # TODO: if game or player is nil
+    if game.full?
+      puts 'full'
+      return redirect_to action: 'index'
+    end
+
+    if game.started?
+      puts 'started'
+      return redirect_to action: 'index'
+    end
+
+    game.game_players.create(player: player, status: 'joined')
 
     redirect_to action: 'show', game_id: game.id
   end
 
   def show
     @game = Game.find_by(id: params[:game_id])
+
+    # TODO: refactor this
     game_players = @game.game_players
     @game_player_first_id = game_players.first ? game_players.first.player_id : 'not yet'
     @game_player_last_id = game_players.last ? game_players.last.player_id : 'not yet'
--- app/models/game.rb
+++ app/models/game.rb
@@ -6,11 +6,15 @@ class Game < ApplicationRecord
   validates :status, presence: true, length: { maximum: 16 }
 
   def full?
-    self.game_players.count == 2
+    self.game_players.where(status: 'joined').count == 2
+  end
+
+  def started?
+    self.status == 'started'
   end
 
   def start
-    self.update(status: 'playing')
-    self.game_histories.create(game_data: {status: 'start'}, start_time: Time.now)
+    self.update(status: 'started')
+    self.game_histories.create(game_data: {status: 'started'}, start_time: Time.now)
   end
 end
