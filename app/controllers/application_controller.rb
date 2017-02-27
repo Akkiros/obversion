9bceea1da5b6e5af883a2eaceb423d6bcd92a526
Akkiros <kwj96966@gmail.com>
2017-02-27 16:25:42 +0700

    global exception catch 추가

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from Exception, with: :global_exception
 
   def require_login
     if session[:player_id].nil?
@@ -12,5 +13,12 @@ class ApplicationController < ActionController::Base
       flash[:notice] = 'player is nil'
       return redirect_to accounts_path
     end
+  end
+
+  private
+
+  def global_exception(e)
+    Rails.logger.error("Exception : #{e.message}")
+    redirect_back(fallback_location: accounts_path)
   end
 end
